9c36d7f4c6ff38ef9780a0e37875ffa77582ba86
Abdulla Khalil Ebrahim Isa Ali <201601016@student.polytechnic.bh>
2018-12-02 21:10:50 +0300

    changed layout

--- Desktop/Desktop/MainMenu.Designer.cs
+++ Desktop/Desktop/MainMenu.Designer.cs
@@ -28,189 +28,202 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label2 = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.button6 = new System.Windows.Forms.Button();
-			this.button7 = new System.Windows.Forms.Button();
-			this.button8 = new System.Windows.Forms.Button();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// label2
-			// 
-			this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
-			this.label2.Dock = System.Windows.Forms.DockStyle.Top;
-			this.label2.Location = new System.Drawing.Point(0, 0);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(626, 62);
-			this.label2.TabIndex = 0;
-			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
-			this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
-			this.label1.ForeColor = System.Drawing.Color.DarkRed;
-			this.label1.Location = new System.Drawing.Point(12, 17);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(200, 33);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "ACEM College";
-			// 
-			// button1
-			// 
-			this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
-			this.button1.FlatAppearance.BorderSize = 0;
-			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button1.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button1.ForeColor = System.Drawing.Color.LightCyan;
-			this.button1.Location = new System.Drawing.Point(32, 132);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(117, 35);
-			this.button1.TabIndex = 3;
-			this.button1.Text = "Instructors";
-			this.button1.UseVisualStyleBackColor = false;
-			// 
-			// button2
-			// 
-			this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button2.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button2.ForeColor = System.Drawing.Color.LightCyan;
-			this.button2.Location = new System.Drawing.Point(171, 132);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(176, 35);
-			this.button2.TabIndex = 4;
-			this.button2.Text = "Courses";
-			this.button2.UseVisualStyleBackColor = false;
-			// 
-			// button3
-			// 
-			this.button3.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button3.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button3.ForeColor = System.Drawing.Color.LightCyan;
-			this.button3.Location = new System.Drawing.Point(353, 132);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(117, 35);
-			this.button3.TabIndex = 5;
-			this.button3.Text = "Sections";
-			this.button3.UseVisualStyleBackColor = false;
-			// 
-			// button4
-			// 
-			this.button4.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button4.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button4.ForeColor = System.Drawing.Color.LightCyan;
-			this.button4.Location = new System.Drawing.Point(32, 197);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(117, 35);
-			this.button4.TabIndex = 6;
-			this.button4.Text = "students";
-			this.button4.UseVisualStyleBackColor = false;
-			// 
-			// button5
-			// 
-			this.button5.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button5.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button5.ForeColor = System.Drawing.Color.LightCyan;
-			this.button5.Location = new System.Drawing.Point(476, 132);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(117, 35);
-			this.button5.TabIndex = 7;
-			this.button5.Text = "Schedules";
-			this.button5.UseVisualStyleBackColor = false;
-			// 
-			// button6
-			// 
-			this.button6.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button6.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button6.ForeColor = System.Drawing.Color.LightCyan;
-			this.button6.Location = new System.Drawing.Point(353, 197);
-			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(117, 35);
-			this.button6.TabIndex = 8;
-			this.button6.Text = "Locations";
-			this.button6.UseVisualStyleBackColor = false;
-			// 
-			// button7
-			// 
-			this.button7.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button7.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button7.ForeColor = System.Drawing.Color.LightCyan;
-			this.button7.Location = new System.Drawing.Point(171, 198);
-			this.button7.Name = "button7";
-			this.button7.Size = new System.Drawing.Size(176, 34);
-			this.button7.TabIndex = 9;
-			this.button7.Text = "Taught Courses";
-			this.button7.UseVisualStyleBackColor = false;
-			// 
-			// button8
-			// 
-			this.button8.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button8.Font = new System.Drawing.Font("Tahoma", 15F);
-			this.button8.ForeColor = System.Drawing.Color.LightCyan;
-			this.button8.Location = new System.Drawing.Point(476, 197);
-			this.button8.Name = "button8";
-			this.button8.Size = new System.Drawing.Size(117, 35);
-			this.button8.TabIndex = 10;
-			this.button8.Text = "Reports";
-			this.button8.UseVisualStyleBackColor = false;
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(35, 296);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(50, 13);
-			this.label3.TabIndex = 11;
-			this.label3.Text = "Today is ";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(86, 296);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(0, 13);
-			this.label4.TabIndex = 12;
-			// 
-			// MainMenu
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(626, 329);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.button8);
-			this.Controls.Add(this.button7);
-			this.Controls.Add(this.button6);
-			this.Controls.Add(this.button5);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.label2);
-			this.Name = "MainMenu";
-			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "ACEM College 2018";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(939, 136);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(13, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ACEM College";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button1.ForeColor = System.Drawing.Color.LightCyan;
+            this.button1.Location = new System.Drawing.Point(42, 247);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Instructors";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button2.ForeColor = System.Drawing.Color.LightCyan;
+            this.button2.Location = new System.Drawing.Point(333, 247);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Courses";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button3.ForeColor = System.Drawing.Color.LightCyan;
+            this.button3.Location = new System.Drawing.Point(193, 455);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 51);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sections";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button4.ForeColor = System.Drawing.Color.LightCyan;
+            this.button4.Location = new System.Drawing.Point(42, 351);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(266, 51);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "students";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button5.ForeColor = System.Drawing.Color.LightCyan;
+            this.button5.Location = new System.Drawing.Point(631, 247);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(264, 51);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Schedules";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button6.ForeColor = System.Drawing.Color.LightCyan;
+            this.button6.Location = new System.Drawing.Point(631, 350);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(264, 51);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Locations";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button7.ForeColor = System.Drawing.Color.LightCyan;
+            this.button7.Location = new System.Drawing.Point(333, 351);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(264, 50);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Taught Courses";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.button8.ForeColor = System.Drawing.Color.LightCyan;
+            this.button8.Location = new System.Drawing.Point(484, 455);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(264, 51);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Reports";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 617);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Today is ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 617);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.TabIndex = 12;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 645);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainMenu";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ACEM College 2018";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
