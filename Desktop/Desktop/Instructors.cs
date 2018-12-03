a95068722e0ebbd8c2b32d45400f643590a2ab48
Moosa Jaffar Abdulla Isa Hammad <201600954@student.polytechnic.bh>
2018-12-03 21:02:33 +0300

    some changes

---
+++ Desktop/Desktop/Instructors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop
+{
+    public partial class Instructors : Form
+    {
+        public Instructors()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form addInstructorForm = new AddInstructor();
+            addInstructorForm.Show();
+            addInstructorForm.Location = this.Location;
+        }
+    }
+}
--- Desktop/Desktop/MainMenu.Designer.cs
+++ Desktop/Desktop/MainMenu.Designer.cs
@@ -47,9 +47,8 @@
             this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(939, 136);
+            this.label2.Size = new System.Drawing.Size(626, 93);
             this.label2.TabIndex = 0;
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
@@ -59,10 +58,9 @@
             this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.DarkRed;
-            this.label1.Location = new System.Drawing.Point(13, 69);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(9, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 48);
+            this.label1.Size = new System.Drawing.Size(200, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "ACEM College";
             // 
@@ -74,13 +72,13 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button1.ForeColor = System.Drawing.Color.LightCyan;
-            this.button1.Location = new System.Drawing.Point(42, 247);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(28, 169);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(264, 51);
+            this.button1.Size = new System.Drawing.Size(176, 35);
             this.button1.TabIndex = 3;
             this.button1.Text = "Instructors";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -88,10 +86,9 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button2.ForeColor = System.Drawing.Color.LightCyan;
-            this.button2.Location = new System.Drawing.Point(333, 247);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(222, 169);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(264, 51);
+            this.button2.Size = new System.Drawing.Size(176, 35);
             this.button2.TabIndex = 4;
             this.button2.Text = "Courses";
             this.button2.UseVisualStyleBackColor = false;
@@ -102,10 +99,9 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button3.ForeColor = System.Drawing.Color.LightCyan;
-            this.button3.Location = new System.Drawing.Point(193, 455);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(129, 311);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(266, 51);
+            this.button3.Size = new System.Drawing.Size(177, 35);
             this.button3.TabIndex = 5;
             this.button3.Text = "Sections";
             this.button3.UseVisualStyleBackColor = false;
@@ -116,10 +112,9 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button4.ForeColor = System.Drawing.Color.LightCyan;
-            this.button4.Location = new System.Drawing.Point(42, 351);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(28, 240);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(266, 51);
+            this.button4.Size = new System.Drawing.Size(177, 35);
             this.button4.TabIndex = 6;
             this.button4.Text = "students";
             this.button4.UseVisualStyleBackColor = false;
@@ -130,10 +125,9 @@
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button5.ForeColor = System.Drawing.Color.LightCyan;
-            this.button5.Location = new System.Drawing.Point(631, 247);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(421, 169);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(264, 51);
+            this.button5.Size = new System.Drawing.Size(176, 35);
             this.button5.TabIndex = 7;
             this.button5.Text = "Schedules";
             this.button5.UseVisualStyleBackColor = false;
@@ -144,10 +138,9 @@
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button6.ForeColor = System.Drawing.Color.LightCyan;
-            this.button6.Location = new System.Drawing.Point(631, 350);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Location = new System.Drawing.Point(421, 239);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(264, 51);
+            this.button6.Size = new System.Drawing.Size(176, 35);
             this.button6.TabIndex = 8;
             this.button6.Text = "Locations";
             this.button6.UseVisualStyleBackColor = false;
@@ -158,10 +151,9 @@
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button7.ForeColor = System.Drawing.Color.LightCyan;
-            this.button7.Location = new System.Drawing.Point(333, 351);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Location = new System.Drawing.Point(222, 240);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(264, 50);
+            this.button7.Size = new System.Drawing.Size(176, 34);
             this.button7.TabIndex = 9;
             this.button7.Text = "Taught Courses";
             this.button7.UseVisualStyleBackColor = false;
@@ -172,10 +164,9 @@
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button8.ForeColor = System.Drawing.Color.LightCyan;
-            this.button8.Location = new System.Drawing.Point(484, 455);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Location = new System.Drawing.Point(323, 311);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(264, 51);
+            this.button8.Size = new System.Drawing.Size(176, 35);
             this.button8.TabIndex = 10;
             this.button8.Text = "Reports";
             this.button8.UseVisualStyleBackColor = false;
@@ -183,27 +174,25 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 617);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(9, 422);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.Size = new System.Drawing.Size(50, 13);
             this.label3.TabIndex = 11;
             this.label3.Text = "Today is ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(95, 617);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(63, 422);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.Size = new System.Drawing.Size(0, 13);
             this.label4.TabIndex = 12;
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(939, 645);
+            this.ClientSize = new System.Drawing.Size(626, 441);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button8);
@@ -216,7 +205,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainMenu";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
