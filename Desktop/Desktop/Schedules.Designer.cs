98bfd9c1f5e1819afed83348629f5495e91b49e8
Moosa Jaffar Abdulla Isa Hammad <201600954@student.polytechnic.bh>
2018-12-17 00:42:18 +0300

    The Total Value Report

--- Desktop/Desktop/Schedules.Designer.cs
+++ Desktop/Desktop/Schedules.Designer.cs
@@ -43,10 +43,9 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button3.ForeColor = System.Drawing.Color.LightCyan;
-            this.button3.Location = new System.Drawing.Point(389, 220);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(259, 143);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(264, 54);
+            this.button3.Size = new System.Drawing.Size(176, 35);
             this.button3.TabIndex = 23;
             this.button3.Text = "Exit";
             this.button3.UseVisualStyleBackColor = false;
@@ -58,10 +57,9 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.DarkRed;
-            this.label1.Location = new System.Drawing.Point(18, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 48);
+            this.label1.Size = new System.Drawing.Size(137, 33);
             this.label1.TabIndex = 22;
             this.label1.Text = "Schedule";
             // 
@@ -73,10 +71,9 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button2.ForeColor = System.Drawing.Color.LightCyan;
-            this.button2.Location = new System.Drawing.Point(389, 109);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(259, 71);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(264, 54);
+            this.button2.Size = new System.Drawing.Size(176, 35);
             this.button2.TabIndex = 21;
             this.button2.Text = "Edit Schedule";
             this.button2.UseVisualStyleBackColor = false;
@@ -90,10 +87,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button1.ForeColor = System.Drawing.Color.LightCyan;
-            this.button1.Location = new System.Drawing.Point(64, 109);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(43, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(264, 54);
+            this.button1.Size = new System.Drawing.Size(176, 35);
             this.button1.TabIndex = 20;
             this.button1.Text = "Add Schedule";
             this.button1.UseVisualStyleBackColor = false;
@@ -107,10 +103,9 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Tahoma", 15F);
             this.button4.ForeColor = System.Drawing.Color.LightCyan;
-            this.button4.Location = new System.Drawing.Point(64, 220);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(43, 143);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(264, 54);
+            this.button4.Size = new System.Drawing.Size(176, 35);
             this.button4.TabIndex = 24;
             this.button4.Text = "Delete Schedule";
             this.button4.UseVisualStyleBackColor = false;
@@ -118,17 +113,17 @@
             // 
             // Schedules
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 323);
+            this.ClientSize = new System.Drawing.Size(483, 210);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Schedules";
             this.Text = "Schedules";
+            this.Load += new System.EventHandler(this.Schedules_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
