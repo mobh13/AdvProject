3255b3fd529a1bd7a4051af03018f7c0273c675b
Moosa Jaffar Abdulla Isa Hammad <201600954@student.polytechnic.bh>
2018-12-17 19:12:41 +0300

    Delete Functionality For Section

--- Desktop/Desktop/DeleteSection.cs
+++ Desktop/Desktop/DeleteSection.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DB;
 
 namespace Desktop
 {
     public partial class DeleteSection : Form
     {
+        SectionList sectionList;
+        SectionStudentList sectionStudentList;
+        ScheduleList scheduleList;
+
         public DeleteSection()
         {
             InitializeComponent();
@@ -19,7 +24,49 @@ namespace Desktop
 
         private void button3_Click(object sender, EventArgs e)
         {
+            this.Close();
+        }
 
+        private void DeleteSection_Load(object sender, EventArgs e)
+        {
+            sectionList = new SectionList();
+            sectionStudentList = new SectionStudentList();
+            scheduleList = new ScheduleList();
+            PopulateSections();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete this section?",
+               "Delete Notice", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Section section = (Section)cmbSectionID.SelectedItem;
+                //Delete All Related Records First
+                //From SectionStudent Table
+                sectionStudentList.Delete("SectionID", cmbSectionID.SelectedItem.ToString());
+                //From Schedule Table
+                scheduleList.Delete("SectionID", cmbSectionID.SelectedItem.ToString());
+                //Delete Section From Section Table Second
+                sectionList.Delete(section);
+
+                if (section.getValid() == true)
+                {
+                    MessageBox.Show("Section has been deleted successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("An error has occured. record was not added.");
+                }
+
+            }
+            PopulateSections();
+        }
+
+        private void PopulateSections()
+        {
+            sectionList.Populate();
+            cmbSectionID.DataSource = sectionList.List;
         }
     }
 }
