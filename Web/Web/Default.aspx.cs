a8eee64819b666584772a8fa2087b4475c36d583
Madan <201601310@student.polytechnic.bh>
2018-12-17 17:32:33 +0300

    1-added comments for student mainpage and records, logout , default. 2-edited the bisnuss rules of the enroll functionality 3- please don't remove my connection string just comment it , thank you.

--- Web/Web/Default.aspx.cs
+++ Web/Web/Default.aspx.cs
@@ -10,87 +10,120 @@ namespace Web
 {
 	public partial class _Default : Page
 	{
-		StudentList students;
-		InstructorList instructors;
+		/* 
+		 declare Variables to be used in this page
+		*/
+		protected StudentList students;
+		protected InstructorList instructors;
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			/*
+			 instantiate classes when the page loads 
+			 */
 			students = new StudentList();
 			instructors = new InstructorList();
 		}
 		protected void LoginButton_Click(object sender, EventArgs e)
 		{
+			/*
+			 * when the button login clicked run this code
+			 * 
+			 */
 
-			
-			 if (lg_username.Text == "" || lg_password.Text == "")
+			if (lg_username.Text == "" || lg_password.Text == "")
 			{
+				/*
+				 * if the username or password are empty then show error message
+				 */
 				errormsg.Text = "Please Complete your account information";
 				meassgaePanel.Visible = true;
 
 			}
-			else if(!instructorAccount.Checked && !studentAccount.Checked)
+			else if (!instructorAccount.Checked && !studentAccount.Checked)
 			{
+				/* 
+				 * if non of the account type radio buttons checked then show error message
+				 */
 				errormsg.Text = "Please Choose  your account type";
 				meassgaePanel.Visible = true;
 
 			}
-			else if (instructorAccount.Checked && lg_username.Text!="" && lg_password.Text!="")
+			else if (instructorAccount.Checked && lg_username.Text != "" && lg_password.Text != "")
 			{
-				meassgaePanel.Visible = false;
-				instructors.Filter("InstructorID", lg_username.Text.ToString());
-				if (instructors.List.Count == 1)
+				/* 
+				 * if the user checked the instructor and the username and password aren't empty then check for information and do the login
+				 */
+
+				meassgaePanel.Visible = false; // hide the message panel
+				instructors.Filter("InstructorID", lg_username.Text.ToString()); // filter the instructors list with the id the user filed
+				if (instructors.List.Count == 1) // if the id is correct the instructors list count should be 1
 				{
-					Instructor instructor = (Instructor) instructors.List.ElementAt(0);
-					instructors.Populate(instructor);
-					if (instructor.Password == lg_password.Text.ToString())
+					Instructor instructor = (Instructor) instructors.List.ElementAt(0); // get the instructor object from the list
+					instructors.Populate(instructor); // populate the instructor 
+					if (instructor.Password == lg_password.Text.ToString()) // if the password user filled is the same as the instructor object
 					{
-						Session["User"] = instructor.getID();
-						Session["Account"] = "Instructor";
-						Response.Redirect("~/instructor/MainPage.aspx");
+						Session["User"] = instructor.getID(); // create new session called User with the value of the id of the instructor
+						Session["Account"] = "Instructor"; // create new Session called Account with  instructor as value this to make sure that the user dosen't visit pages not allwoed for him/her to visit
+						Response.Redirect("~/instructor/MainPage.aspx"); // redirect to main page of the instructor
 
 					}
 					else
 					{
+						/* 
+						 * else show error message "password Wrong"
+						 */
 						errormsg.Text = "Wrong password, Please check your password and try again";
 						meassgaePanel.Visible = true;
 					}
 				}
 				else
 				{
+					/* 
+					 * else show error message "User not found"
+					 */
 					errormsg.Text = "User Not Found, Please check your userId and account type and try again ";
 					meassgaePanel.Visible = true;
 				}
-			
-			}
-			else if(studentAccount.Checked && lg_username.Text != "" && lg_password.Text != "")
-			{
-				meassgaePanel.Visible = false;
 
-				students.Filter("StudentID", lg_username.Text);
-				if (students.List.Count == 1)
+			}
+			else if (studentAccount.Checked && lg_username.Text != "" && lg_password.Text != "")
+			{
+				/* 
+				 * if the user checked the student and the username and password aren't empty then check for information and do the login
+				 */
+				meassgaePanel.Visible = false; // hide the message panel
+				students.Filter("StudentID", lg_username.Text); // filter the students list with the id the user filed
+				if (students.List.Count == 1) // if the id is correct the students list count should be 1
 				{
-					Student student = (Student)students.List.ElementAt(0);
-					students.Populate(student);
-					if (student.Password == lg_password.Text)
+					Student student = (Student) students.List.ElementAt(0); // get the student object from the list
+					students.Populate(student); // populate the student 
+					if (student.Password == lg_password.Text)// if the password user filled is the same as the student object
 					{
-						Session["User"] = student.getID();
-						Session["Account"] = "Student";
-						Response.Redirect("~/student/MainPage.aspx");
+						Session["User"] = student.getID(); // create new session called User with the value of the id of the student
+						Session["Account"] = "Student"; // create new Session called Account with student as value this to make sure that the user dosen't visit pages not allwoed for him/her to visit
+						Response.Redirect("~/student/MainPage.aspx"); // Redirect to student's main page
 					}
 					else
 					{
+						/* 
+						 * else show error message "password Wrong"
+						 */
 						errormsg.Text = "Wrong password, Please check your password and try again";
 						meassgaePanel.Visible = true;
 					}
 				}
 				else
 				{
+					/* 
+					 * else show error message "User not found"
+					 */
 					errormsg.Text = "User Not Found, Please check your userId and account type and try again ";
 					meassgaePanel.Visible = true;
 				}
 
 			}
-			
+
 		}
-		
+
 	}
 }
--- Web/Web/student/ClassList.aspx.cs
+++ Web/Web/student/ClassList.aspx.cs
@@ -5,12 +5,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
- using DB;
+using DB;
 
 namespace Web.student
 {
 	public partial class ClassList : System.Web.UI.Page
 	{
+		/* 
+		 * declare Variables to be used in this page
+		 */
 		protected SectionList sectionsList;
 		protected TaughtCourseList taughtList;
 		protected CourseList courseList;
@@ -18,40 +21,65 @@ namespace Web.student
 		protected InstructorList instructorList;
 		protected LocationList locationList;
 		protected StudentList studentList;
+		protected SectionStudentList sectionStudentList;
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			if (Session["User"] != null && Session["Account"].ToString() == "Student")
 			{
+				/*
+				 * check if the user session exist and Account session value is student
+				 */
+
+				/*
+			instantiate classes when the page loads 
+			*/
 				sectionsList = new SectionList();
 				taughtList = new TaughtCourseList();
 				locationList = new LocationList();
 				courseList = new CourseList();
 				scheduleList = new ScheduleList();
-				 studentList = new StudentList();
-				scheduleList.Populate();
+				sectionStudentList = new SectionStudentList();
+				studentList = new StudentList();
+				scheduleList.Populate(); // populate the sechuleList
 				instructorList = new InstructorList();
-				GenerateGridData();
+				GenerateGridData();// call GenerateGridData method to fill the gridview element with data
 
 			}
 			else if (Session["User"] != null && Session["Account"].ToString() != "Student")
 			{
+				/*
+				 * else if the user session exist but the account session value is not student show error message 
+				 */
 				Response.Write("Error You are not allowed to be here");
 				Response.End();
 			}
 			else
-			{
+			{	
+				/*
+				 * else show error message
+				 */
 				Response.Write("Error please log in before trying to Access this page..!");
 				Response.End();
 
 			}
-			
-			
+
+
 
 		}
+		/*
+		 * Method: GenerateGridData
+		 * Areguments: none
+		 * Return: none
+		 * Des: this method is to fill the GridView with data
+		 */
 		protected void GenerateGridData()
 		{
-			DataTable table = new DataTable();
+			DataTable table = new DataTable();// declare and instantiate DataTable object
+
+											  /*
+											   * Add Columns to the data table
+											   */
 			table.Columns.Add("Course Id");
 			table.Columns.Add("Section Id");
 			table.Columns.Add("Course Title");
@@ -61,27 +89,25 @@ namespace Web.student
 			table.Columns.Add("Instructor Name");
 			foreach (Schedule schedule in scheduleList.List)
 			{
+				/*
+				 * Foreach Element in the scheduleList  
+				 */
+				sectionsList.Filter("SectionID", schedule.SectionID); // filter the sectionList using the section id from the element of scheduleList
+				Section section = (Section) sectionsList.List.ElementAt(0); // get the section object from the sectionList
 
-				sectionsList.Filter("SectionID", schedule.SectionID);
-				Section section = (Section)sectionsList.List.ElementAt(0);
-
-				taughtList.Filter("TaughtCourseID", section.TaughtCourseID);
-				TaughtCourse taughtcourse = (TaughtCourse)taughtList.List.ElementAt(0);
-				taughtList.Populate(taughtcourse);
-				courseList.Filter("CourseID", taughtcourse.CourseID);
-				Course course = (Course)courseList.List.ElementAt(0);
-				courseList.Populate(course);
-				locationList.Filter("LocationID", schedule.LocationID);
-				Location location = (Location)locationList.List.ElementAt(0);
-				locationList.Populate(location);
-				instructorList.Filter("InstructorID", section.InstructorID);
-				Instructor instructor = (Instructor)instructorList.List.ElementAt(0);
-				instructorList.Populate(instructor);
+				taughtList.Filter("TaughtCourseID", section.TaughtCourseID); // filter the taughtCourseList usign the taughtCourse id from the section object
+				TaughtCourse taughtcourse = (TaughtCourse) taughtList.List.ElementAt(0); // get the taughtCourse object from the list
+				courseList.Filter("CourseID", taughtcourse.CourseID); //filter the courseList using the CourseId from the taughtCourse object
+				Course course = (Course) courseList.List.ElementAt(0); // get the course object fom the couseList
+				locationList.Filter("LocationID", schedule.LocationID); //filter the locationList using the location id from the element
+				Location location = (Location) locationList.List.ElementAt(0); // get location object 
+				instructorList.Filter("InstructorID", section.InstructorID); // filter the instructor id form the 
+				Instructor instructor = (Instructor) instructorList.List.ElementAt(0); // get the instructor object form the instructorList
 				table.Rows.Add(taughtcourse.CourseID, section.getID(), course.Title, location.Name, schedule.Time, schedule.Day, instructor.FirstName + " " + instructor.LastName);
-
+				// add a row to the dataTable object with the information from the above objects
 			}
-			classListGrid.DataSource = table;
-			classListGrid.DataBind();
+			classListGrid.DataSource = table; // set the dataTable object as the dataSource of the GidView element
+			classListGrid.DataBind(); // bind the data
 		}
 		protected void EnrollMeBtn_Click(object sender, EventArgs e)
 		{
@@ -94,7 +120,7 @@ namespace Web.student
 			Section section = new Section(sectionId);
 			sectionsList.Populate(section);
 			Student student = new Student(studentID);
-			
+
 			studentList.Populate(student);
 			int totalHours = scheduleList.TotalValue("Duration", "Section", "Schedule.SectionID", "Section.SectionID", "SectionStudent", "Section.SectionID", "SectionStudent.SectionID", "StudentID", student.getID());
 			int sectionTotalHours = scheduleList.TotalValue("Duration", "SectionID", sectionId);
@@ -118,11 +144,11 @@ namespace Web.student
 						{
 							int newTime = Convert.ToInt32(schedule.Time) + 1;
 							schedule.Time = newTime.ToString();
-							 exist = scheduleList.Exist("Section", "Schedule.SectionID", "Section.SectionID", "SectionStudent", "Section.SectionID", "SectionStudent.SectionID", "Day", schedule.Day, "Time", schedule.Time, "StudentID", student.getID());
+							exist = scheduleList.Exist("Section", "Schedule.SectionID", "Section.SectionID", "SectionStudent", "Section.SectionID", "SectionStudent.SectionID", "Day", schedule.Day, "Time", schedule.Time, "StudentID", student.getID());
 							int duration = Convert.ToInt32(schedule.Duration) - 1;
 							schedule.Duration = duration.ToString();
 						}
-					
+
 						if (exist)
 						{
 							alreadyExist = true;
@@ -136,20 +162,26 @@ namespace Web.student
 					}
 					else
 					{
-						int total = sectionsList.TotalValue("StudentID", "SectionID", sectionId);
+						sectionStudentList.Filter("SectionID", sectionId);
+						int total = sectionStudentList.List.Count;
 						total = total + 1;
 						Schedule schedule = new Schedule(scheduleID);
 						scheduleList.Populate(schedule);
 						Location location = new Location(schedule.LocationID);
 						locationList.Populate(location);
-						if(Convert.ToInt32(location.Capacity) >= total){
+						if (Convert.ToInt32(location.Capacity) >= total)
+						{
 							SectionStudent sectionStudent = new SectionStudent(sectionId, studentID);
 							SectionStudentList sectionStudentList = new SectionStudentList();
 							sectionStudentList.Add(sectionStudent);
 							ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Section Added", "alert('You have been Enrolled to this section !')", true);
 						}
+						else
+						{
+							ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Error", "alert('The Section is Already Full !')", true);
 
-						ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Error", "alert('The Section is Already Full !')", true);
+						}
+
 
 					}
 				}
